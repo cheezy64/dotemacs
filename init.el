4d2fb475e5439e54a41fe6da8da00e5b25279908
Che <cheezy64@gmail.com>
2015-03-30 00:33:43 -0500

    Remove idle from use-package since it's not supported in 2.0+.  Also fixed up yasnippet

--- init.el
+++ init.el
@@ -269,33 +269,6 @@
   :config
   (progn
     (yas-load-directory "~/.emacs.d/snippets/")
-
-    (bind-key "C-i" 'yas-next-field-or-maybe-expand yas-keymap)
-
-    (defun yas-new-snippet (&optional choose-instead-of-guess)
-      (interactive "P")
-      (let ((guessed-directories (yas-guess-snippet-directories)))
-        (switch-to-buffer "*new snippet*")
-        (erase-buffer)
-        (kill-all-local-variables)
-        (snippet-mode)
-        (set (make-local-variable 'yas-guessed-modes)
-             (mapcar #'(lambda (d)
-                         (intern (yas-table-name (car d))))
-                     guessed-directories))
-        (unless (and choose-instead-of-guess
-                     (not (y-or-n-p "Insert a snippet with useful headers? ")))
-          (yas-expand-snippet "\
-# -*- mode: snippet -*-
-# name: $1
-# --
-$0"))))
-
-    (bind-key "C-c y TAB" 'yas-expand)
-    (bind-key "C-c y n" 'yas-new-snippet)
-    (bind-key "C-c y f" 'yas-find-snippets)
-    (bind-key "C-c y r" 'yas-reload-all)
-    (bind-key "C-c y v" 'yas-visit-snippet-file)
     (yas-reload-all)
 ))
 
@@ -312,7 +285,6 @@ $0"))))
 
 (use-package semantic
   :commands (semantic-mode helm-semantic-or-imenu)
-  :idle (semantic-mode)
   :config
   (progn
     (global-semanticdb-minor-mode 1)
